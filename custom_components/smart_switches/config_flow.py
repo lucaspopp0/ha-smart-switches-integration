dbce15bda66ed3326319fde9b9e7cfb576f43b51
lucaspopp0 <lucaspopp0@gmail.com>
2025-05-29 21:05:32 -0700

    basic config flow

--- custom_components/smart_switches/config_flow.py
+++ custom_components/smart_switches/config_flow.py
@@ -1,11 +1,8 @@
-from homeassistant import data_entry_flow
-from homeassistant.data_entry_flow import section
-from homeassistant.helpers.selector import selector
-import voluptuous as vol
+from homeassistant import config_entries
 
 DOMAIN="smart_switches"
 
-class ExampleConfigFlow(data_entry_flow.FlowHandler, domain=DOMAIN):
+class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Example config flow."""
     # The schema version of the entries that it creates
     # Home Assistant will call your migrate method if the version changes
