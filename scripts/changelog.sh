00f2efe7587b2e8b5869dba7f7bf3ca7f838f207
Lucas Popp <lucaspopp0@gmail.com>
2025-05-29 19:28:44 -0700

    workflows (#2)

---
+++ scripts/changelog.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+if [[ -n "$VERSION_TAG" ]]; then
+    echo "# ${VERSION_TAG}";
+fi
+
+if [[ -n "$COMMIT_MESSAGE" ]]; then
+    first=true
+    while IFS='' read -r line || [[ -n "$line" ]]; do
+        if [[ "$first" == "true" ]]; then
+            first=false
+            echo " * $line"
+        else
+            echo "   $line"
+        fi
+    done <<< "$COMMIT_MESSAGE"
+
+    echo ""
+fi
+
+git log HEAD --format=' * %h %s' \
+    | sed -E 's/.*\[bot\] (.+)/\n# \1/'
