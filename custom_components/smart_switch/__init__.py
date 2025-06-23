32d4b3fd61e0ff1ffb8d5ca637f4f88f81e38f9a
Lucas Popp <lucaspopp0@gmail.com>
2025-06-22 20:59:27 -0700

    config flow update?

--- custom_components/smart_switch/__init__.py
+++ custom_components/smart_switch/__init__.py
@@ -2,35 +2,59 @@
 
 from __future__ import annotations
 
-from homeassistant.config_entries import ConfigEntry
+import logging
+
 from homeassistant.const import Platform
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.components import bluetooth
+from homeassistant.exceptions import ConfigEntryNotReady
+
+from .coordinator import (SmartSwitchConfigEntry, SmartSwitchDataUpdateCoordinator)
+from .const import DOMAIN
 
 _PLATFORMS: list[Platform] = [Platform.SENSOR]
 
-@callback
-def _async_discovered_device(service_info: bluetooth.BluetoothServiceInfoBleak, change: bluetooth.BluetoothChange) -> None:
-    """Subscribe to bluetooth changes."""
-    _LOGGER.warning("New service_info: %s", service_info)
+_LOGGER = logging.getLogger(__name__)
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: SmartSwitchConfigEntry) -> bool:
     """Set up Smart Switches from a config entry."""
 
-    # TODO 1. Create API instance
-    # TODO 2. Validate the API connection (and authentication)
-    # TODO 3. Store an API object for your platforms to access
-    # entry.runtime_data = MyAPI(...)
+    assert entry.unique_id is not None
 
-    entry.async_on_unload(
-        bluetooth.async_register_callback(
-            hass,
-            _async_discovered_device,
-            {"service_uuid": "DF82C9F9-D7B4-47F0-914F-788B3B2AB9A1", "connectable": True},
-            bluetooth.BluetoothScanningMode.ACTIVE,
-        )
+    address: str = entry.data['address']
+
+    ble_device = bluetooth.async_ble_device_from_address(
+        hass, address.upper(), True,
     )
 
-    await hass.config_entries.async_forward_entry_setups(entry, _PLATFORMS)
+    if not ble_device:
+        raise ConfigEntryNotReady(
+            translation_domain=DOMAIN,
+            translation_key="device_not_found_error",
+        )
 
-    return True
+    coordinator = entry.runtime_data = SmartSwitchDataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        ble_device,
+        entry.unique_id,
+        entry.data.get('name', entry.title),
+        True,
+    )
+
+    entry.async_on_unload(coordinator.async_start())
+    if not await coordinator.async_wait_ready():
+        raise ConfigEntryNotReady(
+            translation_domain=DOMAIN,
+            translation_key="advertising_error"
+        )
+    
+    entry.async_on_unload(entry.add_update_listener(_async_update_listener))
+    await hass.config_entries.async_forward_entry_setups(
+        entry, Platform.SENSOR,
+    )
+
+async def _async_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Handle options update."""
+    await hass.config_entries.async_reload(entry.entry_id)
--- custom_components/smart_switch/config_flow.py
+++ custom_components/smart_switch/config_flow.py
@@ -8,14 +8,24 @@ from typing import Any
 import voluptuous as vol
 
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
+from homeassistant.data_entry_flow import AbortFlow
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import config_entry_flow
+
+from homeassistant.components.bluetooth import (
+    BluetoothServiceInfoBleak,
+    async_discovered_service_info,
+)
 
 from collections import OrderedDict
 
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
+
+def format_unique_id(address: str) -> str:
+    """Format the UUId of a smart switch from its Bluetooth MAC address"""
+    return address.replace(":", "").lower()
 
 class SmartSwitchConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Smart Switches."""
@@ -25,14 +35,93 @@ class SmartSwitchConfigFlow(ConfigFlow, domain=DOMAIN):
     def __init__(self) -> None:
         """Initialize the config flow."""
         self.id = DOMAIN
+        self.discovered_info: BluetoothServiceInfoBleak | None = None
+        self.discovered_infos: dict[str, BluetoothServiceInfoBleak] = {}
+
+    async def _async_set_device(self, discovery_info: BluetoothServiceInfoBleak) -> None:
+        """Set the device to work with"""
+        self.discovered_info = discovery_info
+        await self.async_set_unique_id(
+            format_unique_id(discovery_info.address),
+            raise_on_progress=False,
+        )
+        self._abort_if_unique_id_configured()
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
-        """Handle the initial step."""
+        """Handle the user step for picking discovered devices"""
         if user_input is not None:
-            await self.async_set_unique_id(DOMAIN)
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(title="Smart Switches", data={})
+            discovery_info = self.discovered_infos[user_input['address']]
+            await self._async_set_device(discovery_info)
+            return await self._async_create_entry_from_discovery(user_input)
 
-        return self.async_show_form(step_id="user")
+        self._discover_devices()
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required('address'): vol.In(
+                        {
+                            address: discovery_info.name
+                            for address, discovery_info in self.discovered_infos.items()
+                        }
+                    )
+                }
+            )
+        )
+    
+    def _discover_devices(self) -> None:
+        current_addresses = self._async_current_ids(include_ignore=False)
+        for connectable in (True, False):
+            for discovery_info in async_discovered_service_info(self.hass, connectable):
+                address = discovery_info.address
+                if (
+                    format_unique_id(address) in current_addresses
+                    or address in self.discovered_infos
+                ):
+                    continue
+
+                print(discovery_info)
+
+                if discovery_info.connectable:
+                    self.discovered_infos = discovery_info
+
+        if not self.discovered_infos:
+            raise AbortFlow("no_devices_found")
+        
+    async def async_step_bluetooth(
+            self, discovery_info: BluetoothServiceInfoBleak,
+    ) -> ConfigFlowResult:
+        """Bluetooth discovery step"""
+        _LOGGER.debug("Discovered bluetooth device: %s", discovery_info.as_dict())
+        await self._async_set_device(discovery_info)
+        return await self.async_step_confirm()
+
+    async def async_step_confirm(
+            self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Confirms a single device"""
+        assert self.discovered_info is not None
+        if user_input is not None:
+            return await self._async_create_entry_from_discovery(user_input)
+            
+        self._set_confirm_only()
+        return self.async_show_form(
+            step_id="confirm",
+            data_schema=vol.Schema({}),
+            description_placeholders={
+                "name": self.discovered_info.name,
+            }
+        )
+
+    async def _async_create_entry_from_discovery(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Create an entry from a discovery"""
+        assert self.discovered_info is not None
+        return self.async_create_entry(
+            title=self.discovered_info.name,
+            data=self.discovered_info,
+        )
+    
